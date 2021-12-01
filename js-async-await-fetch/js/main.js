9d5f8124159afb119367d58bf14ddc4033fd8913
Edgar Garcia <90708343+LeoG11@users.noreply.github.com>
2021-11-30 23:31:49 -0600

    Update main.js

--- js-async-await-fetch/js/main.js
+++ js-async-await-fetch/js/main.js
@@ -1,10 +1,11 @@
 let RegistrosHTML = document.getElementById('RegistrosHTML');
 
 let arrayRegistros = [];
-
+const puppeteer = require('puppeteer');
 //Comunicación con el servidor
 async function leeRegistros() {
-
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
     const respuesta = await fetch("http://bdd-elga-01.signsle4u.xyz/index.php");
     const registros = await respuesta.json();
 
@@ -21,6 +22,7 @@ async function muestraRegistros() {
 
     console.log(arrayRegistros);
     RegistrosHTML.innerHTML = RegistrosHTML.innerHTML + "Dirección IP -> " + arrayRegistros.ip + " " + "<br>" + "Pais -> " + arrayRegistros.pais + "<br>";
+    await browser.close();
    
 }
 
